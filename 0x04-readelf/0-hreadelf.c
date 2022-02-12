2baa4aad5c45f536ae65ec199ff587a4d2b4e95b
samilo-del <camilovalenciapadilla@gmail.com>
2022-02-12 17:49:29 +0000

    add

--- 0x04-readelf/0-hreadelf.c
+++ 0x04-readelf/0-hreadelf.c
@@ -1,20 +1,103 @@
-#include "helf.h"
+#include "_elf.h"
 
 /**
- * main - calls parse_args and printelfh
- * @argc: number of arguments
- * @argv: pointer to array of characters containing arguments
- *
- * Return: 0 on success, otherwise 1
+ * print_magic_bytes - Prints magic bytes of elf, first 16 bytes
+ * @bytes: character array
  */
-int main(int argc, char **argv)
+void print_magic_bytes(unsigned char *bytes)
 {
-	FILE *fp;
-	int exit_stat;
+	size_t i;
+	unsigned char *ident = ((Elf64_Ehdr *)bytes)->e_ident;
 
-	fp = parse_args(argc, argv);
-	if (!fp)
-		return (1);
-	exit_stat = printelfh(fp, *argv);
-	return (exit_stat);
+	printf("  Magic:   ");
+	for (i = 0; i < 16; ++i)
+	{
+		printf("%02x ", ident[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * print_string_table_idx - Print Section header string table index
+ * @bytes: character array
+ * @class: ELFCLASS32 or ELFCLASS64
+ * @endianess: LSB or MSB
+ */
+void print_string_table_idx(unsigned char *bytes, int class, int endianess)
+{
+	uint16_t shstrndx = class == ELFCLASS32 ?
+		((Elf32_Ehdr *) bytes)->e_shstrndx :
+		((Elf64_Ehdr *) bytes)->e_shstrndx;
+
+	if (endianess == ELFDATA2MSB)
+		reverse((unsigned char *) &shstrndx, 2);
+
+	printf("  Section header string table index: %d\n", shstrndx);
+}
+
+/**
+ * print_elf_header - Print all info from elf header
+ * @bytes: character array, where the header is stored
+ * @class: ELFCLASS32 or ELFCLASS64 (32 or 64 bit)
+ * @endianess: LSB or MSB
+ */
+void print_elf_header(unsigned char *bytes, int class, int endianess)
+{
+	printf("ELF Header:\n");
+	print_magic_bytes(bytes);
+	print_class(bytes);
+	print_data(bytes);
+	print_version(bytes);
+	print_osabi(bytes);
+	print_abi_version(bytes);
+	print_type(bytes, endianess);
+	print_machine(bytes, endianess);
+	print_file_version(bytes, endianess);
+	print_entry_point_addr(bytes, class, endianess);
+	print_program_headers(bytes, class, endianess);
+	print_section_headers(bytes, class, endianess);
+	print_flags(bytes, class, endianess);
+	print_header_size(bytes, class, endianess);
+	print_program_header_size(bytes, class, endianess);
+	print_num_program_headers(bytes, class, endianess);
+	print_section_hsize(bytes, class, endianess);
+	print_num_section_headers(bytes, class, endianess);
+	print_string_table_idx(bytes, class, endianess);
+}
+
+/**
+ * main - Entry point, output is similar to readelf -W -h
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	unsigned char bytes[64];
+
+	if (argc != 2)
+		return (EXIT_SUCCESS);
+	if (access(argv[1], F_OK) == -1)
+	{
+		fprintf(stderr, "readelf: Error: '%s': No such file\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
+	if (access(argv[1], R_OK) == -1)
+	{
+		fprintf(stderr,
+			"readelf: Error: %s: Failed to read file's magic number\n",
+			argv[1]);
+		return (EXIT_FAILURE);
+	}
+	if (read_elf_header_bytes(bytes, argv[1]))
+	{
+		perror("readelf: Error: ");
+		return (EXIT_FAILURE);
+	}
+	if (check_elf(bytes))
+		return (EXIT_FAILURE);
+	print_elf_header(bytes,
+		     bytes[4] == ELFCLASS32 ? ELFCLASS32 : ELFCLASS64,
+		     bytes[5] == ELFDATA2MSB ? ELFDATA2MSB : ELFDATA2LSB);
+	return (0);
 }
--- 0x04-readelf/1-hreadelf.c
+++ 0x04-readelf/1-hreadelf.c
@@ -1,144 +1,115 @@
-#include "helf.h"
-
-static int is_32;
-static int is_msb;
+#include "_elf.h"
 
 /**
- * offset - prints the section header offset
- * @hdr: struct containing elf header information
- * @is_32: specifies whether ELF file is 32-bit
- * @is_msb: specifies if ELF file is big endian
- *
- * Return: section header offset
+ * print_section_header_loop - Print info about section headers
+ * reads each section in a loop
+ * @bytes: character array
+ * @class: ELFCLASS32 or ELFCLASS64
+ * @endianess: LSB or MSB
+ * @n_sections: number of sections
+ * @header_size: size of section header
+ * @str_table: string table section, .shtstrtab
  */
-int offset(hdrs hdr, int is_32, int is_msb)
+void print_section_header_loop(unsigned char *bytes, int class, int endianess,
+			       int n_sections, int header_size, unsigned char *str_table)
 {
-	Elf32_Off offset32 = hdr.Ehdr32.e_shoff;
-	Elf64_Off offset64 = hdr.Ehdr64.e_shoff;
+	int i;
 
-	if (is_32)
+	print_section_header_names(class);
+
+	for (i = 0; i < n_sections; ++i, bytes += header_size)
 	{
-		if (is_msb)
-			convertmsb((char *) &offset32, sizeof(offset32));
-		return (offset32);
+		printf("  [%2d] ", i);
+		print_section_name(bytes, class, endianess, str_table);
+		print_section_type(bytes, class, endianess);
+		print_section_addr(bytes, class, endianess);
+		print_section_off(bytes, class, endianess);
+		print_section_size(bytes, class, endianess);
+		print_section_es(bytes, class, endianess);
+		print_section_flg(bytes, class, endianess);
+		print_section_lk(bytes, class, endianess);
+		print_section_inf(bytes, class, endianess);
+		print_section_al(bytes, class, endianess);
 	}
-	else
-	{
-		if (is_msb)
-			convertmsb((char *) &offset64, sizeof(offset64));
-		return (offset64);
-	}
+
+	print_key_to_flags(class);
 }
 
 /**
- * shnum - gets the number of section headers
- * @hdr: struct containing elf header information
- * @is_32: specifies whether ELF file is 32-bit
- * @is_msb: specifies if ELF file is big endian
- *
- * Return: number of section headers
+ * print_section_headers - Print info about section headers, like readelf -S -W
+ * @bytes: character array
+ * @filename: elf file
+ * @class: ELFCLASS32 or ELFCLASS64 (32 or 64 bit)
+ * @endianess: LSB or MSB
  */
-int shnum(hdrs hdr, int is_32, int is_msb)
+void print_section_header(unsigned char *bytes, char *filename, int class,
+			  int endianess)
 {
-	Elf32_Half shnum32 = hdr.Ehdr32.e_shnum;
-	Elf64_Half shnum64 = hdr.Ehdr64.e_shnum;
+	void *p = get_section_header_off(bytes, class, endianess);
+	Elf64_Off offset = class == ELFCLASS32 ? *(Elf32_Off *) p : *(Elf64_Off *) p;
+	uint16_t n_sections = get_num_section_headers(bytes, class, endianess);
+	uint16_t header_size = get_section_hsize(bytes, class, endianess);
+	unsigned char *data = NULL, *str_table = NULL;
+	size_t s_off, s_size;
+	uint16_t shstrndx;
 
-	if (is_32)
+	printf("There are %d section headers, starting at offset %#lx:\n\n",
+	       n_sections, offset);
+
+	if (read_bytes(&data, filename, offset, n_sections * header_size))
 	{
-		if (is_msb)
-			convertmsb((char *) &shnum32, sizeof(shnum32));
-		return (shnum32);
+		exit(EXIT_FAILURE);
 	}
-	else
+
+	shstrndx = get_string_table_idx(bytes, class, endianess);
+	s_off = get_section_off(data + (shstrndx * header_size), class, endianess);
+	s_size = get_section_size(data + (shstrndx * header_size), class, endianess);
+
+	if (read_bytes(&str_table, filename, s_off, s_size))
 	{
-		if (is_msb)
-			convertmsb((char *) &shnum64, sizeof(shnum64));
-		return (shnum64);
+		free(data);
+		exit(EXIT_FAILURE);
 	}
+	print_section_header_loop(data, class, endianess,
+				  n_sections, header_size, str_table);
+	free(data);
+	free(str_table);
 }
 
 /**
- * checkbitend - checks system bit and endian
- * @hdr: struct containing elf header information
- * @arg_str: string containing name of program
- *
- * Return: 0 on success, otherwise 1
+ * main - Entry point, output is similar to readelf -S -W
+ * prints section header
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 otherwise
  */
-int checkbitend(hdrs hdr, char *arg_str)
+int main(int argc, char *argv[])
 {
-	if (hdr.Ehdr64.e_ident[EI_CLASS] == ELFCLASSNONE)
+	unsigned char bytes[64];
+
+	if (argc != 2)
+		return (EXIT_SUCCESS);
+	if (access(argv[1], F_OK) == -1)
 	{
-		fprintf(stderr, "%s: Error: Invalid class\n", arg_str);
-		goto out;
+		fprintf(stderr, "readelf: Error: '%s': No such file\n", argv[1]);
+		return (EXIT_FAILURE);
 	}
-	if (hdr.Ehdr64.e_ident[EI_DATA] == ELFDATANONE)
+	if (access(argv[1], R_OK) == -1)
 	{
-		fprintf(stderr, "%s: Error: Invalid data encoding\n", arg_str);
-out:
-		return (1);
+		fprintf(stderr,
+			"readelf: Error: %s: Failed to read file's magic number\n",
+			argv[1]);
+		return (EXIT_FAILURE);
 	}
-	if (hdr.Ehdr64.e_ident[EI_CLASS] == ELFCLASS32)
-		is_32 = 1;
-	if (hdr.Ehdr64.e_ident[EI_DATA] == ELFDATA2MSB)
-		is_msb = 1;
+	if (read_elf_header_bytes(bytes, argv[1]))
+	{
+		perror("readelf: Error: ");
+		return (EXIT_FAILURE);
+	}
+	if (check_elf(bytes))
+		return (EXIT_FAILURE);
+	print_section_header(bytes, argv[1],
+			 bytes[4] == ELFCLASS32 ? ELFCLASS32 : ELFCLASS64,
+			 bytes[5] == ELFDATA2MSB ? ELFDATA2MSB : ELFDATA2LSB);
 	return (0);
-}
-
-/**
- * printelfsh - prints elf header
- * @fp: pointer to file stream
- * @arg_str: string containing name of program
- *
- * Return: 0 on success, otherwise 1
- */
-int printelfsh(FILE *fp, char *arg_str)
-{
-	hdrs hdr;
-	int exit_stat;
-
-	fread(&hdr.Ehdr64, sizeof(hdr.Ehdr64), 1, fp);
-	rewind(fp);
-	fread(&hdr.Ehdr32, sizeof(hdr.Ehdr32), 1, fp);
-	if (memcmp(hdr.Ehdr64.e_ident, ELFMAG, SELFMAG))
-	{
-		fprintf(stderr, "%s: %s%s\n", arg_str,
-				"Error: Not an ELF file - ",
-				"it has the wrong magic bytes at the start");
-		exit_stat = 1;
-		goto out;
-	}
-	exit_stat = checkbitend(hdr, arg_str);
-	if (exit_stat)
-		goto out;
-	fseek(fp, hdr.Ehdr64.e_shoff, SEEK_SET);
-	fread(&hdr.Shdr64, sizeof(hdr.Shdr64), 1, fp);
-	fseek(fp, hdr.Ehdr32.e_shoff, SEEK_SET);
-	fread(&hdr.Shdr32, sizeof(hdr.Shdr32), 1, fp);
-	printf("There are %i section headers, starting at offset %#x:\n\n",
-			shnum(hdr, is_32, is_msb), offset(hdr, is_32, is_msb));
-	exit_stat = 0;
-out:
-	fclose(fp);
-	if (exit_stat)
-		return (1);
-	return (0);
-}
-
-/**
- * main - calls parse_args and printelfsh
- * @argc: number of arguments
- * @argv: pointer to array of characters containing arguments
- *
- * Return: 0 on success, otherwise 1
- */
-int main(int argc, char **argv)
-{
-	FILE *fp;
-	int exit_stat;
-
-	fp = parse_args(argc, argv);
-	if (!fp)
-		return (1);
-	exit_stat = printelfsh(fp, *argv);
-	return (exit_stat);
 }
